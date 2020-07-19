0aeba3815d7ea347d512f9b955c765b169c0d5c1
DESKTOP-EUTU0IO\Lenovo <bouraoui.mansouri@gmail.com>
2020-07-19 19:03:47 +0100

    first commit

---
+++ project 2cinfo/Rest-post-Angular-master/README.md
@@ -0,0 +1,3 @@
+# Angular Rest 
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.3.1.
---
+++ project 2cinfo/Rest-post-Angular-master/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { DeletePostComponent } from './delete-post/delete-post.component';
+import { LoginComponent } from './login/login.component';
+import {AuthService} from './auth.service';
+import {PostService} from './post.service';
+import {AuthGuard} from './auth.guard';
+import {Routes, RouterModule} from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  {path: '', component : HomeComponent},
+  {path: 'login', component : LoginComponent},
+  {path: 'posts', component : PostComponent, canActivate : [AuthGuard]},
+  {path: 'add-post', component : AddPostComponent , canActivate : [AuthGuard]},
+  {path: 'edit-post/:id', component : EditPostComponent , canActivate : [AuthGuard]},
+  {path: 'main', component : MainComponent, canActivate : [AuthGuard]},
+  ];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent,
+    AddPostComponent,
+    EditPostComponent,
+    DeletePostComponent,
+    LoginComponent,
+    HomeComponent,
+    MainComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot( routes ),
+    HttpModule,
+    FormsModule
+  ],
+  providers: [AuthService, PostService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
